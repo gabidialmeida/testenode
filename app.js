2cd049199520fe5b509d4312f5837888be17fc1c
GabiAlmeida <gabi.almeida.souza25@gmail.com>
2019-01-02 15:03:31 -0200

    views e inicio de modulos

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 var express=require('express');//recuperando a função
 var app=express();//recuperando retorno
+
+var msg= require('./mod_teste');
+app.set('view engine','ejs');  //indicar que o ejs vai ser o responsavel pela geração de view
+
 app.get('/', function(req,res)
 {
-    res.send("<html><body>Inicio</body></html>");
-})
-app.get('/tecnologia', function(req,res)
+    res.render("home"); // caso exista um arquivo com o nome index na pasta e não seja indicado um arquivo na função render, será buscada a ´página INDEX, caso nao, dará erro
+});
+
+/*app.get('/tecnologia', function(req,res)
 {
     res.send("<html><body>Tecnologia</body></html>");
-})
+});*/ //antes de usar o ejs
+
+app.get('/tecnologia', function(req,res)
+{
+    res.render("secao/tecnologia");//passar arquivo que será buscado pelo ejs / não preciso indicar que meu arquivo é do tipo ejs, pois o view engine já foi configurado para esse tipo de arquivo
+});
+
+app.get('/noticia', function(req,res)
+{
+    res.render("noticias/noticias");
+});
+
+app.get('/add', function(req,res)
+{
+    res.render("admin/form_add_noticia");
+});
 app.listen(3000, function()
 {
-    console.log("Rodando com Express");
-})
+    console.log("Rodando com Express. ");
+    console.log(msg());
+
+});
---
+++ mod_teste.js
@@ -0,0 +1,17 @@
+//primeiro modulo. para retornar uma string
+/*function teste(){
+    var msg ="esse modulo so retorna uma string";
+    return msg;
+}
+module.exports= teste();
+*/
+
+//var teste="testeee";
+module.exports=function teste(){
+    var msg ="esse modulo so retorna uma string";
+    return msg;
+};
+module.exports=function teste2(){
+    var msg ="teste2";
+    return msg;
+};
